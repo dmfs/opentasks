6971df73d43029c27ab4dd943dd2d9ef81c1b915
Arjun Naik <arjun@arjunnaik.in>
2013-01-25 10:21:28 +0100

    Added a SelectionFieldView

---
+++ src/org/dmfs/tasks/widget/SelectionFieldView.java
@@ -0,0 +1,32 @@
+package org.dmfs.tasks.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Field view to display multiple selection items.
+ * @author Arjun Naik<arjun@arjunnaik.in>
+ *
+ */
+
+public class SelectionFieldView extends AbstractFieldView {
+
+	public SelectionFieldView(Context context) {
+		super(context);
+	}
+	
+	public SelectionFieldView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+	
+	public SelectionFieldView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+	@Override
+	protected void updateView() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
