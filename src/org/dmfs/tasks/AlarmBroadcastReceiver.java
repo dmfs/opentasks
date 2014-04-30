c2d65cad0e6fa85192c7366c250a0a04f447f27c
tobias <tobias@dmfs.org>
2014-04-30 16:01:38 +0200

    - added support for start alarms

--- src/org/dmfs/tasks/AlarmBroadcastReceiver.java
+++ src/org/dmfs/tasks/AlarmBroadcastReceiver.java
@@ -3,7 +3,8 @@ package org.dmfs.tasks;
 import java.net.URI;
 
 import org.dmfs.provider.tasks.TaskContract.Tasks;
-import org.dmfs.provider.tasks.handler.AlarmNotificationHandler;
+import org.dmfs.provider.tasks.broadcast.DueAlarmBroadcastHandler;
+import org.dmfs.provider.tasks.broadcast.StartAlarmBroadcastHandler;
 
 import android.app.Notification;
 import android.app.NotificationManager;
@@ -38,45 +39,91 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver
 	public void onReceive(Context context, Intent intent)
 	{
 		// continue if alarms where enabled
-		if (getAlarmPreference(context))
+		if (intent.getAction().equals(StartAlarmBroadcastHandler.BROADCAST_START_ALARM))
 		{
-			long taskId = intent.getLongExtra(AlarmNotificationHandler.EXTRA_TASK_ID, 0);
-			// long dueTime = intent.getLongExtra(AlarmNotificationHandler.EXTRA_TASK_DUE_TIME, System.currentTimeMillis());
-			String title = intent.getStringExtra(AlarmNotificationHandler.EXTRA_TASK_TITLE);
+			if (getAlarmPreference(context))
+			{
+				long taskId = intent.getLongExtra(StartAlarmBroadcastHandler.EXTRA_TASK_ID, 0);
+				// long dueTime = intent.getLongExtra(AlarmNotificationHandler.EXTRA_TASK_DUE_TIME, System.currentTimeMillis());
+				String title = intent.getStringExtra(StartAlarmBroadcastHandler.EXTRA_TASK_TITLE);
 
-			NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+				NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-			// build notification
-			NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context).setSmallIcon(R.drawable.ic_notification_completed)
-				.setContentTitle(context.getString(R.string.notification_task_due_title)).setContentText(title);
+				// build notification
+				NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context).setSmallIcon(R.drawable.ic_notification_completed)
+					.setContentTitle(context.getString(R.string.notification_task_start_title)).setContentText(title);
 
-			// dismisses the notification on click
-			mBuilder.setAutoCancel(true);
+				// dismisses the notification on click
+				mBuilder.setAutoCancel(true);
 
-			// set status bar test
-			mBuilder.setTicker(title);
+				// set status bar test
+				mBuilder.setTicker(title);
 
-			// enable light, sound and vibration
-			mBuilder.setDefaults(Notification.DEFAULT_ALL);
+				// enable light, sound and vibration
+				mBuilder.setDefaults(Notification.DEFAULT_ALL);
 
-			// Creates an explicit intent for an Activity in your app
-			Intent resultIntent = new Intent(context, ViewTaskActivity.class);
-			resultIntent.setData(getUriForTask(taskId));
+				// Creates an explicit intent for an Activity in your app
+				Intent resultIntent = new Intent(context, ViewTaskActivity.class);
+				resultIntent.setData(getUriForTask(taskId));
 
-			// The stack builder object will contain an artificial back stack for the
-			// started Activity.
-			// This ensures that navigating backward from the Activity leads out of
-			// your application to the Home screen.
-			TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-			// Adds the back stack for the Intent (but not the Intent itself)
-			stackBuilder.addParentStack(ViewTaskActivity.class);
-			// Adds the Intent that starts the Activity to the top of the stack
-			stackBuilder.addNextIntent(resultIntent);
-			PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+				// The stack builder object will contain an artificial back stack for the
+				// started Activity.
+				// This ensures that navigating backward from the Activity leads out of
+				// your application to the Home screen.
+				TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+				// Adds the back stack for the Intent (but not the Intent itself)
+				stackBuilder.addParentStack(ViewTaskActivity.class);
+				// Adds the Intent that starts the Activity to the top of the stack
+				stackBuilder.addNextIntent(resultIntent);
+				PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
-			mBuilder.setContentIntent(resultPendingIntent);
-			notificationManager.notify((int) taskId, mBuilder.build());
+				mBuilder.setContentIntent(resultPendingIntent);
+				notificationManager.notify((int) taskId, mBuilder.build());
 
+			}
+		}
+		else if (intent.getAction().equals(DueAlarmBroadcastHandler.BROADCAST_DUE_ALARM))
+		{
+			if (getAlarmPreference(context))
+			{
+				long taskId = intent.getLongExtra(DueAlarmBroadcastHandler.EXTRA_TASK_ID, 0);
+				// long dueTime = intent.getLongExtra(AlarmNotificationHandler.EXTRA_TASK_DUE_TIME, System.currentTimeMillis());
+				String title = intent.getStringExtra(DueAlarmBroadcastHandler.EXTRA_TASK_TITLE);
+
+				NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+				// build notification
+				NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context).setSmallIcon(R.drawable.ic_notification_completed)
+					.setContentTitle(context.getString(R.string.notification_task_due_title)).setContentText(title);
+
+				// dismisses the notification on click
+				mBuilder.setAutoCancel(true);
+
+				// set status bar test
+				mBuilder.setTicker(title);
+
+				// enable light, sound and vibration
+				mBuilder.setDefaults(Notification.DEFAULT_ALL);
+
+				// Creates an explicit intent for an Activity in your app
+				Intent resultIntent = new Intent(context, ViewTaskActivity.class);
+				resultIntent.setData(getUriForTask(taskId));
+
+				// The stack builder object will contain an artificial back stack for the
+				// started Activity.
+				// This ensures that navigating backward from the Activity leads out of
+				// your application to the Home screen.
+				TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+				// Adds the back stack for the Intent (but not the Intent itself)
+				stackBuilder.addParentStack(ViewTaskActivity.class);
+				// Adds the Intent that starts the Activity to the top of the stack
+				stackBuilder.addNextIntent(resultIntent);
+				PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+				mBuilder.setContentIntent(resultPendingIntent);
+				notificationManager.notify((int) taskId, mBuilder.build());
+
+			}
 		}
 
 	}
