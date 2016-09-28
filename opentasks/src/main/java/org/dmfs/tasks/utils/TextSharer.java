a06670a6333e085ef85e6463e60be213a493d908
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2016-09-28 16:16:34 +0200

    Finalize text sharer intent flags.

--- opentasks/src/main/java/org/dmfs/tasks/utils/TextSharer.java
+++ opentasks/src/main/java/org/dmfs/tasks/utils/TextSharer.java
@@ -23,14 +23,8 @@ public class TextSharer
     {
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-
-        /* TODO
-          figure the usage of these extras out,
-         ideally to be able to use with email (subject+body) and simple full text as well*/
-        sendIntent.putExtra(Intent.EXTRA_TITLE, title);
         sendIntent.putExtra(Intent.EXTRA_SUBJECT, title);
         sendIntent.putExtra(Intent.EXTRA_TEXT, body);
-
         sendIntent.setType("text/plain");
         mContext.startActivity(sendIntent);
     }
