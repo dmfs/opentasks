123dae09839b409d5c0e5bc672e272af762b8a47
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2016-10-03 16:55:19 +0200

    Rename a new layout to use 'opentasks' prefix.

--- opentasks/src/main/java/org/dmfs/tasks/EmptyTaskFragment.java
+++ opentasks/src/main/java/org/dmfs/tasks/EmptyTaskFragment.java
@@ -36,7 +36,7 @@ public class EmptyTaskFragment extends SupportFragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return inflater.inflate(R.layout.fragment_empty_task, container, false);
+        return inflater.inflate(R.layout.opentasks_fragment_empty_task, container, false);
     }
 
 }
