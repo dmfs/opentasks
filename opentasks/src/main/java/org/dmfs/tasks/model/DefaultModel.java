010b9d381003a37e8968d58484c14ffb3e87d638
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2017-03-30 00:34:54 +0200

    Implement location field opening maps with new LocationFieldView and opentasks_location_field_view.xml layout. (#285)

--- opentasks/src/main/java/org/dmfs/tasks/model/DefaultModel.java
+++ opentasks/src/main/java/org/dmfs/tasks/model/DefaultModel.java
@@ -34,6 +34,7 @@ public class DefaultModel extends Model
 {
 	final static LayoutDescriptor TEXT_VIEW = new LayoutDescriptor(R.layout.text_field_view).setOption(LayoutDescriptor.OPTION_LINKIFY, Linkify.ALL);
 	final static LayoutDescriptor TEXT_VIEW_NO_LINKS = new LayoutDescriptor(R.layout.text_field_view).setOption(LayoutDescriptor.OPTION_LINKIFY, 0);
+	private final static LayoutDescriptor LOCATION_VIEW = new LayoutDescriptor(R.layout.opentasks_location_field_view).setOption(LayoutDescriptor.OPTION_LINKIFY, 0);
 	private final static LayoutDescriptor TEXT_EDIT = new LayoutDescriptor(R.layout.text_field_editor);
 	private final static LayoutDescriptor TEXT_EDIT_SINGLE_LINE = new LayoutDescriptor(R.layout.text_field_editor).setOption(LayoutDescriptor.OPTION_MULTILINE,
 		false);
@@ -101,7 +102,7 @@ public class DefaultModel extends Model
 			.setEditorLayout(CHOICES_EDIT).setChoices(aca).setIcon(R.drawable.ic_detail_status));
 
 		// location
-		addField(new FieldDescriptor(context, R.id.task_field_location, R.string.task_location, TaskFieldAdapters.LOCATION).setViewLayout(TEXT_VIEW)
+		addField(new FieldDescriptor(context, R.id.task_field_location, R.string.task_location, TaskFieldAdapters.LOCATION).setViewLayout(LOCATION_VIEW)
 			.setEditorLayout(TEXT_EDIT).setIcon(R.drawable.ic_detail_location));
 
 		// description
