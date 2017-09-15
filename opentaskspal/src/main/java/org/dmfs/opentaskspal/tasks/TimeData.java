0447ed2e979c6d7804a9c5484df9abe60a20707c
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2017-09-15 17:27:17 +0200

    Fix all-day flag value in TimeData.

--- opentaskspal/src/main/java/org/dmfs/opentaskspal/tasks/TimeData.java
+++ opentaskspal/src/main/java/org/dmfs/opentaskspal/tasks/TimeData.java
@@ -114,7 +114,7 @@ public final class TimeData implements RowData<TaskContract.Tasks>
         return builder
                 .withValue(TaskContract.Tasks.DTSTART, start.getTimestamp())
                 .withValue(TaskContract.Tasks.TZ, start.isAllDay() ? "UTC" : start.getTimeZone().getID())
-                .withValue(TaskContract.Tasks.IS_ALLDAY, start.isAllDay())
+                .withValue(TaskContract.Tasks.IS_ALLDAY, start.isAllDay() ? 1 : 0)
 
                 .withValue(TaskContract.Tasks.DUE, due == null ? null : due.getTimestamp())
 
