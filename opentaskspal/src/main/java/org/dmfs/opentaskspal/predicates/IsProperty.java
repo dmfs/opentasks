79e9658bd76af1ab6cba41613d2cb066db1ae9b8
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2017-10-18 14:12:30 +0200

    OpenTaskPal subtask operations and queries support. #450 (#465)

---
+++ opentaskspal/src/main/java/org/dmfs/opentaskspal/predicates/IsProperty.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.opentaskspal.predicates;
+
+import org.dmfs.android.contentpal.Predicate;
+import org.dmfs.android.contentpal.predicates.DelegatingPredicate;
+import org.dmfs.android.contentpal.predicates.EqArg;
+import org.dmfs.tasks.contract.TaskContract;
+
+
+/**
+ * {@link Predicate} for selecting rows from {@link TaskContract.Properties} table representing a certain property identified by its mimeType.
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class IsProperty extends DelegatingPredicate
+{
+    public IsProperty(String mimeType)
+    {
+        super(new EqArg(TaskContract.PropertyColumns.MIMETYPE, mimeType));
+    }
+}
---
+++ opentaskspal/src/main/java/org/dmfs/opentaskspal/tasks/RelationData.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.opentaskspal.tasks;
+
+import android.support.annotation.NonNull;
+
+import org.dmfs.android.contentpal.RowData;
+import org.dmfs.android.contentpal.RowSnapshot;
+import org.dmfs.android.contentpal.rowdata.CharSequenceRowData;
+import org.dmfs.android.contentpal.rowdata.Composite;
+import org.dmfs.android.contentpal.rowdata.DelegatingRowData;
+import org.dmfs.android.contentpal.rowdata.RawRowData;
+import org.dmfs.android.contentpal.rowdata.Referring;
+import org.dmfs.tasks.contract.TaskContract;
+import org.dmfs.tasks.contract.TaskContract.Property.Relation.RelType;
+
+
+/**
+ * {@link RowData} for adding a {@link TaskContract.Property.Relation} property.
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class RelationData extends DelegatingRowData<TaskContract.Properties>
+{
+    public RelationData(@NonNull RowSnapshot<TaskContract.Tasks> relatingTask,
+                        @NonNull RelType relType,
+                        @NonNull RowSnapshot<TaskContract.Tasks> relatedTask)
+    {
+        super(new Composite<>(
+                new Referring<TaskContract.Properties>(TaskContract.Property.Relation.TASK_ID, relatingTask),
+                new RawRowData<TaskContract.Properties>(TaskContract.Property.Relation.RELATED_TYPE, relType.ordinal()),
+                new Referring<TaskContract.Properties>(TaskContract.Property.Relation.RELATED_ID, relatedTask)));
+    }
+
+
+    public RelationData(@NonNull RowSnapshot<TaskContract.Tasks> relatingTask,
+                        @NonNull RelType relType,
+                        @NonNull CharSequence relatedTaskUid)
+    {
+        super(new Composite<>(
+                new Referring<TaskContract.Properties>(TaskContract.Property.Relation.TASK_ID, relatingTask),
+                new RawRowData<TaskContract.Properties>(TaskContract.Property.Relation.RELATED_TYPE, relType.ordinal()),
+                new CharSequenceRowData<TaskContract.Properties>(TaskContract.Property.Relation.RELATED_UID, relatedTaskUid)));
+    }
+}
