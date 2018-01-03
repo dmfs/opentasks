69d47da28c713f1ee9671d1879b517b9d2f047c2
Marten Gajda <marten@dmfs.org>
2018-01-03 19:35:23 +0100

    Add RowData to create recurring tasks. Implements #460 (#604)
    This certainly needs to be revisited, but it should be ok for now when it's mostly required to write unit tests.

---
+++ opentaskspal/src/main/java/org/dmfs/opentaskspal/tasks/ExDatesTaskData.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.opentaskspal.tasks;
+
+import android.support.annotation.NonNull;
+
+import org.dmfs.android.contentpal.RowData;
+import org.dmfs.android.contentpal.rowdata.DelegatingRowData;
+import org.dmfs.opentaskspal.rowdata.DateTimeListData;
+import org.dmfs.rfc5545.DateTime;
+import org.dmfs.tasks.contract.TaskContract;
+
+
+/**
+ * {@link RowData} for tasks with EXDATEs
+ * <p>
+ * TODO: how to make sure this is only ever used with tasks having a start and/or due date?
+ *
+ * @author Marten Gajda
+ */
+public final class ExDatesTaskData extends DelegatingRowData<TaskContract.Tasks>
+{
+    public ExDatesTaskData(@NonNull Iterable<DateTime> exdates)
+    {
+        super(new DateTimeListData<>(TaskContract.Tasks.EXDATE, exdates));
+    }
+}
---
+++ opentaskspal/src/test/java/org/dmfs/opentaskspal/tasks/RRuleTaskDataTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.opentaskspal.tasks;
+
+import org.dmfs.rfc5545.recur.RecurrenceRule;
+import org.dmfs.tasks.contract.TaskContract;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.dmfs.android.contentpal.testing.contentoperationbuilder.WithValues.withValuesOnly;
+import static org.dmfs.android.contentpal.testing.contentvalues.Containing.containing;
+import static org.dmfs.android.contentpal.testing.rowdata.RowDataMatcher.builds;
+import static org.junit.Assert.assertThat;
+
+
+/**
+ * @author Marten Gajda
+ */
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class RRuleTaskDataTest
+{
+    @Test
+    public void test() throws Exception
+    {
+        assertThat(new RRuleTaskData(new RecurrenceRule("FREQ=MONTHLY;BYMONTHDAY=12", RecurrenceRule.RfcMode.RFC2445_LAX)),
+                builds(
+                        withValuesOnly(
+                                containing(TaskContract.Tasks.RRULE, "FREQ=MONTHLY;BYMONTHDAY=12"))));
+
+    }
+
+}
