5b18f8037e33c9f6637e0cc23693fae306e14dd0
Marten Gajda <marten@dmfs.org>
2019-01-11 16:06:19 +0100

    Request account visibility, implements #728 (#736)
    This commit adds a workaround for sync apps which don't grant account visibility to OpenTasks. If a task list is inserted which belongs to an account which we can't see, the app asks for permission to see the account.
    The `GET_ACCOUNTS` permission is no longer used on Android 8+ and the request for that permission will no longer be shown when the app starts.

---
+++ opentaskspal/src/main/java/org/dmfs/opentaskspal/views/TaskListsView.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 dmfs GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.opentaskspal.views;
+
+import android.content.ContentProviderClient;
+
+import org.dmfs.android.contentpal.View;
+import org.dmfs.android.contentpal.views.BaseView;
+import org.dmfs.android.contentpal.views.DelegatingView;
+import org.dmfs.tasks.contract.TaskContract;
+
+
+/**
+ * {@link View} for the {@link TaskContract.TaskLists} table.
+ *
+ * @author Marten Gajda
+ */
+public final class TaskListsView extends DelegatingView<TaskContract.TaskLists>
+{
+    public TaskListsView(String authority, ContentProviderClient client)
+    {
+        super(new BaseView<>(client, TaskContract.TaskLists.getContentUri(authority)));
+    }
+}
